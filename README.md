6eac505b627e423cc1561b5dc52291e418b8c075
Sean McPherson <git@seanmcp.com>
2021-02-12 22:20:48 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# CRA Landing
+
+🛬 Deploy with `create-react-app` _and_ a landing page
---
+++ post-build.sh
@@ -0,0 +1,3 @@
+mkdir build/app/
+mv build/index.html build/app/index.html
+mv build/landing.html build/index.html
